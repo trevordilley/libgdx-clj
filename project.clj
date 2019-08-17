15416afcfb0d082f81b556d78f530490538a10c1
trevordilley <trevor@twentyideas.com>
2019-08-17 07:23:01 -0700

    updated groupid of project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject libgdx-clj/lein-template "0.1.0"
+(defproject org.clojars.trevordilley/libgdx-clj "0.1.0"
   :description "Basic template for a LibGDX game written in Clojure"
   :url "https://github.com/trevordilley/libgdx-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
